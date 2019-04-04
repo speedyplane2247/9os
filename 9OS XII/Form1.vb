669c0e93b15df109bc344960e698d18ce9e12ed0
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-04-04 18:42:27 -0400

    9OS 12 - Developer Preview 1

---
+++ 9OS XII/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Dim BootScreenTick = 0 ' Set the Tick of the Timer to 0.
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        BootScreenTick = BootScreenTick + 1 ' Increase the Tick
+        System.Diagnostics.Debug.WriteLine("BootScreen Tick Occured. " + CStr(BootScreenTick))
+        If BootScreenTick = 2 Then
+            System.Diagnostics.Debug.WriteLine("Got Success! - Booting System!")
+            PreUserArena.Show()
+            System.Diagnostics.Debug.WriteLine("PreUserArena - Loading!")
+            Me.Hide()
+            System.Diagnostics.Debug.WriteLine("We are done here. Hiding Boot Image!")
+        Else
+            ' Do Nothing
+        End If
+    End Sub
+
+    Public Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True ' Start the BootScreen Timer
+    End Sub
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
---
+++ 9OS XII/PreUserArena.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Windows.Forms
+
+Public Class PreUserArena
+    Private Sub PreUserArena_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' Welcome to the PreUserArena - the Area where nothing but the Login Window should run.
+        SystemEventsTimer.Enabled = True
+    End Sub
+
+    Private Sub SystemEventsTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SystemEventsTimer.Tick
+        ' Do something to check here!
+    End Sub
+End Class
